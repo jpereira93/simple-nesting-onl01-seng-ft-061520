019139d0cc4e688251eae6cf5a8927d717ae0102
Jessica Pereira <jcpereira070@gmail.com>
2020-06-21 15:49:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -92,25 +92,25 @@ def adding_matz
 
 end
 
-def changing_alan
-	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL"
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
-
-end
+# def changing_alan
+# 	programmer_hash =
+#  		{
+#         :grace_hopper => {
+#           :known_for => "COBOL"
+#           :languages => ["COBOL", "FORTRAN"]
+#         },
+#         :alan_kay => {
+#           :known_for => "Object Orientation",
+#           :languages => ["Smalltalk", "LISP"]
+#         },
+#         :dennis_ritchie => {
+#           :known_for => "Unix",
+#           :languages => ["C"]
+#         }
+#      }
+# 
+# 
+# end
 
 def adding_to_dennis
 	programmer_hash =
